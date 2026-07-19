b20b6d5109101d28e4b8f0d8fe01295ac2407ea1
nickup8 <nickup8@yandex.ru>
2026-07-19 13:43:01 +0300

    Fix: добавлена защита withoutOverlapping для рассылки напоминаний

--- routes/console.php
+++ routes/console.php
@@ -7,7 +7,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('appointments:reminders')->everyMinute();
+Schedule::command('appointments:reminders')->everyMinute()->withoutOverlapping();
 Schedule::command('appointments:cancel-unpaid')->everyMinute();
 Schedule::command('appointments:cleanup-drafts')->everyFiveMinutes();
 Schedule::command('subscriptions:check-expirations')->dailyAt('00:00');
