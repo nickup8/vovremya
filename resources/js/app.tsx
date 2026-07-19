e842c7c4e92916f2a2ccd764b1058b91c98563a7
nickup8 <nickup8@yandex.ru>
2026-07-19 20:27:49 +0300

    Chore: install Laravel Reverb and configure Echo for WebSockets

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,6 +1,11 @@
 import { createInertiaApp } from '@inertiajs/react';
 import { TooltipProvider } from '@/components/ui/tooltip';
 import { initializeTheme } from '@/hooks/use-appearance';
+import { configureEcho } from '@laravel/echo-react';
+
+configureEcho({
+    broadcaster: 'reverb',
+});
 
 const appName = import.meta.env.VITE_APP_NAME || 'Вовремя';
 
