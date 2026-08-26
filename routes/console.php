e2b3a58a0c61f13bf70e1d27944a517c7e0a1d8b
nickup8 <nickup8@yandex.ru>
2026-08-26 10:06:20 +0300

    feat: add pending marketing consent storage

--- routes/console.php
+++ routes/console.php
@@ -8,3 +8,4 @@ Schedule::command('appointments:cleanup-drafts')->everyFiveMinutes()->withoutOve
 Schedule::command('subscriptions:check-expirations')->dailyAt('00:00');
 Schedule::command('subscriptions:check-limits')->hourly()->withoutOverlapping();
 Schedule::command('subscriptions:cleanup-pending')->hourly()->withoutOverlapping();
+Schedule::command('pending-marketing:cleanup')->hourly()->withoutOverlapping();
