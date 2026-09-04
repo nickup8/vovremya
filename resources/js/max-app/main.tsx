313b50f754857a504a636714b08ac149e0b3de93
nickup8 <nickup8@yandex.ru>
2026-09-04 20:32:51 +0300

    remove max ui dependency from miniapp

--- resources/js/max-app/main.tsx
+++ resources/js/max-app/main.tsx
@@ -1,11 +1,5 @@
-import { MaxUI } from '@maxhub/max-ui';
-import '@maxhub/max-ui/dist/styles.css';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import './styles.css';
 
-createRoot(document.getElementById('max-root')!).render(
-    <MaxUI>
-        <App />
-    </MaxUI>,
-);
+createRoot(document.getElementById('max-root')!).render(<App />);
